b61a79c0cdd265ca6a56fbdc31b8911a744aee9f
Huaxing Zheng <huaxing_zheng@qq.com>
2021-07-24 17:09:39 +0800

    代码重构

--- build_bmfont/index.js
+++ build_bmfont/index.js
@@ -12,7 +12,7 @@ const srcPath = path_1.default.join(argv.src);
 const dstPath = path_1.default.join(argv.dst);
 const wordsTxtPath = path_1.default.join(srcPath, "Words.txt");
 const bmfcPath = path_1.default.join(srcPath, "bmfont.bmfc");
-const qhkFntPath = path_1.default.join(argv.dst, "QHK.fnt");
+const qhkFntPath = path_1.default.join(argv.dst, "HQK.fnt");
 const bmfont64exe = path_1.default.join(__dirname, "bmfont1.14a/bmfont64.exe");;
 
 console.log(">> build_bmfont >> Start");
@@ -31,15 +31,15 @@ let callCmdSync = function (cmd, cwd) {
 
 
 console.log(`---->> 读取动态字库文本 ${wordsTxtPath} >>----`);
-let wordsTxtStr = fs_extra_1.default.readFileSync(wordsTxtPath, { encoding: 'utf8' });
+let wordsTxtStr = fs_extra_1.default.readFileSync(wordsTxtPath, { encoding: 'utf16le' });
 console.log(wordsTxtStr);
 /**
  * 需要转成 utf16le，并且是 crlf 格式
  * https://gist.github.com/zoellner/4af04a5a8b51f04ad653e26d3b7181ec
  */
-console.log("---->> 动态将文本回写 >>----");
-const utf16buffer = Buffer.from(`\ufeff${wordsTxtStr}`, 'utf16le');
-fs_extra_1.default.writeFileSync(wordsTxtPath, utf16buffer);
+// console.log("---->> 动态将文本回写 >>----");
+// const utf16buffer = Buffer.from(`\ufeff${wordsTxtStr}`, 'utf16le');
+// fs_extra_1.default.writeFileSync(wordsTxtPath, utf16buffer);
 
 callCmdSync(`${bmfont64exe} -t ${wordsTxtPath} -c ${bmfcPath}  -o ${qhkFntPath}`, dstPath);
 
